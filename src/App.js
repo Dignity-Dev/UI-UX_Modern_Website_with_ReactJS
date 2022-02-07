dee67f1ff4a186487c65c2e02ae44dfadf552a02
Dignity Dev <bashirhaadiy@gmail.com>
2022-02-07 18:06:52 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React from "react";
 
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import {
+    Footer,
+    Blog,
+    Possibility,
+    Features,
+    WhatGPT3,
+    Header,
+} from "./containers";
 import { CTA, Brand, Navbar } from "./components";
 
-const App = () => {
-    return ( <
-        div className = "App" >
-        <
-        div className = "gradient__bg" >
-        <
-        Navbar / >
-        <
-        Header / >
-        <
-        /div> <
-        Brand / >
-        <
-        WhatGPT3 / >
-        <
-        Features / >
-        <
-        Possibility / >
-        <
-        CTA / >
-        <
-        Blog / >
-        <
-        Footer / >
-        <
-        /div>
-    );
-};
+import "./App.css";
+
+const App = () => ( <
+    div className = "App" >
+    <
+    div className = "gradient__bg" >
+    <
+    Navbar / >
+    <
+    Header / >
+    <
+    /div> <
+    Brand / >
+    <
+    WhatGPT3 / >
+    <
+    Features / >
+    <
+    Possibility / >
+    <
+    CTA / >
+    <
+    Blog / >
+    <
+    Footer / >
+    <
+    /div>
+);
 
 export default App;
